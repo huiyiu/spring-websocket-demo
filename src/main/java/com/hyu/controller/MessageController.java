8919bce883ee2f854c58aee0180e370f9d5b515c
hyu <huiyisn@163.com>
2020-12-29 09:33:36 +0800

    Update MessageController.java
    添加@SendTo使用方法

--- src/main/java/com/hyu/controller/MessageController.java
+++ src/main/java/com/hyu/controller/MessageController.java
@@ -23,5 +23,16 @@ public class MessageController {
         // 将消息发送到 WebSocket 配置类中配置的代理中（/topic）进行消息转发
         simpMessageSendingOperations.convertAndSend("/topic/test", msg);
     }
+    
+     /**
+     *  @SendTo("/topic/test2") === simpMessageSendingOperations.convertAndSend("/topic/test2", msg)
+     * @param msg
+     * @return
+     */
+    @MessageMapping("/test2")
+    @SendTo("/topic/test2")
+    public String sendTopicMessageWithSendTo(String msg) {
+         return msg;
+    }
 
 }
